7863648cf978ba49dd275cd91e90966cf3de647b
itsmechauhan <techhchacker@gamil.com>
2024-04-06 16:45:49 +0530

    fn inside the clss definition

---
+++ fu_inside_class.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class hero{
+public:
+	int health;
+	char level;
+	void print(){
+		cout<<"Health : "<<health<<endl;
+		cout<<"Level: "<<level<<endl;
+			}
+
+};
+
+
+
+int main(){
+hero ramesh;
+ramesh.health=40; 
+ramesh.level='a';
+ramesh.print();
+}
